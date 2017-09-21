71b81dd807ec87781c48400c2a812f906f287334
ping1008 <wxk@ping1008.me>
2017-09-21 19:50:48 +0800

    [Add]Writing interface

---
+++ main.py
@@ -0,0 +1,34 @@
+import os
+import sys
+import time
+import argparse
+
+
+def interface():
+    """The command line interface
+    :returns: TODO
+
+    """
+    print("Latitude and longitude to Address(baidu API)")
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Lati\
+            tude and longitude to Address")
+    parser.add_argument('-i', '--intereactive', action="store_true",
+                        default=False, help="Interactive mode.")
+    parser.add_argument('-rt', '--txt', type=str,
+                        help="Read from txt(see docs for format)")
+    parser.add_argument('-rx', '--xls', type=str, dest="xls",
+                        help="Read from xls")
+    parser.add_argument('-ox', type=str,
+                        help="Write to xls")
+    parser.add_argument('-ot', type=str,
+                        help="Write to txt")
+    args = parser.parse_args()
+    if args.intereactive is False and args.txt is None and args.xls is None:
+        parser.print_help()
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/getpos.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+from ctypes import *
+import os
+import json
+import xlrd
+import requests
+
+"""
+TODO: parse the json obj to py obj
+"""
+
+
+class JSONObject:
+    def __init__(self, d):
+        self.__dict__ = d
+
+
+class MC(Structure):
+    _fields_ = [("x", c_double), ("y", c_double)]
+
+
+def getPos():
+    """TODO: get pos of MC
+
+    :x: TODO
+    :y: TODO
+    :returns: TODO
+
+    """
+    libtest = cdll.LoadLibrary(os.getcwd() + '/mc.so')
+    libtest.convertLL2MC.restype = MC
+    libtest.convertLL2MC.argtypes = (c_double, c_double)
+    # data = xlrd.open_workbook('./newAsn.xls')
+    table = data.sheets()[1]
+    # x = table.col_values(0)
+    del x[0]
+    y = table.col_values(1)
+    del y[0]
+    res = []
+    # print(it.x, it.y)
+    for i in range(0, len(x)):
+        it = libtest.convertLL2MC(y[i], x[i])
+        res.append({"x": it.x, "y": it.y, "qt": "rgc", "dis_poi": 1})
+        r = requests.get('http://api.map.baidu.com/', params=res[i])
+        resTest = json.loads(r.text)
+        # print(resTest)
+        resUltimateJson = json.loads(r.text, object_hook=JSONObject)
+        # print(resUltimateJson)
+        print(resUltimateJson.content.address)
+
+
+if __name__ == "__main__":
+    getPos()
